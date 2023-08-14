69773b500ebd89d0ea8f659416730b0bb0337da4
Zenzo1 <d.zhang9707@gmail.com>
2023-08-14 22:51:30 +1200

    fixed riskcalc

--- api.js
+++ api.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
-const path = require('path') // Add this line
+const path = require('path')
 const port = 8000
 
 // Serve static files (HTML, CSS, JavaScript) from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Import your functions from logic.js
 const { priceCalc, riskCalc, quoteCalc } = require('./logic.ts')
 const { log } = require('console')
 
-app.use(express.json()) // Enable JSON parsing for request bodies
+app.use(express.json())
 
 app.post('/index', (req, res) => {
   const modYea = JSON.stringify(req.body.modYea)
